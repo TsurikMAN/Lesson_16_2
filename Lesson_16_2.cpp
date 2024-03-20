9923fa8b3b437f6891c9471d6bb615b785b11240
Alex Tsurikov <alextsurikman@gmail.com>
2024-03-21 01:15:44 +0300

    Changes

--- Lesson_16_2.cpp
+++ Lesson_16_2.cpp
@@ -12,7 +12,7 @@ int main()
 {
 	
 	
-	const int N=10;
+	const int N=22;
 
 	int array[N][N];
 	
@@ -32,15 +32,18 @@ int main()
 	time_t t;
 	time(&t); 
 	int day = localtime(&t)->tm_mday;
-	int line = (day % N)+1;
+	int line = day % N;
+	int line1 = (day % N) + 1;
+	int line2 = day/N;
 	
 	cout << "The date is: " << day <<endl;
-	cout << "Date%Count of lines = " << line<<endl;
-	cout << "Sum of "<<line<<" line: ";
+	cout << day << "/" << N<<" = " << line2 << " and "<<line1<<" modulo" << endl;
+	cout << "We choose the " << line1<<" line" << endl;
+	cout << "Sum of "<<line1<<" line: ";
 
 	for (int x = 0; x < N; x++)
 		{	
-		sum += array[day % N][x];
+		sum += array[line][x];
 		}
 		
 	for (int k = 0; k < N; k++)
